3c4ce61e7c7dab2378d465954c2299411920309b
Glitch (fossil-capri) <none>
2019-09-11 16:53:45 +0000

    🎙🚙 Checkpoint ./server.js:6079345/189

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/api/exercise/log', (req,res)=>{
         
         // console.log(new Date(req.query.from).toISOString())
           
-          ExerciseLog.find({ _id: req.query.userId }, { "log": { $elemMatch : { "$toDate" : new Date(req.query.from).toIsoString()}}}
+          ExerciseLog.find({ _id: req.query.userId }, { "log": { $elemMatch : { "$date" : { date:  new Date().setHours(00,00,00)}}}}
                            ).exec( (err, data) => {
              err?err: res.send("query data" + "<br />"+ data);
            })
